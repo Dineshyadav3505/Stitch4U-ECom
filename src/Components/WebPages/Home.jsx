d2bc590f3d30fe54a105c583262dda9660f7ebfa
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-14 12:14:55 +0530

    Ecom frontend setup

---
+++ src/Components/WebPages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className="  font-1   " >Home</div>
+    <div className=" font-2   " >Home</div>
+    <div className=" " >Home</div>
+
+    </>
+  )
+}
+
+export default Home
