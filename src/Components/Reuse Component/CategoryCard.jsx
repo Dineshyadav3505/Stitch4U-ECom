9b8365ff961ce7f61f84ff43828a554b5a9d5413
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-19 18:08:04 +0530

    Refactor component props and styles

--- src/Components/Reuse Component/CategoryCard.jsx
+++ src/Components/Reuse Component/CategoryCard.jsx
@@ -4,14 +4,14 @@ import { NavLink } from 'react-router-dom'
 const CategoryCard = ({
     id,
     img="https://www.snitch.co.in/cdn/shop/files/4MST2375-01-M24.jpg?v=1718775027&width=720",
-    Category="POLos"
+    Category="POLo "
 
 }) => {
   return (
-    <NavLink to={`product/${id}`} className=' relative h-56 w-40 rounded-lg overflow-hidden '>
+    <NavLink to={`product/${id}`} className=' relative my-3 h-56 w-40 md:h-64 md:w-48 rounded-lg overflow-hidden '>
         <div className="w-full h-full overflow-hidden">
             <img className='w-full h-full object-fill' src={`${img}`} alt="" />
-            <h3 className= 'absolute bottom-3 left-3 text-white font-bol font-1 text-sm uppercase'>{`${Category}`}</h3>
+            <h3 className= 'absolute bottom-3 left-3 text-white font-bol font-1 text-sm lg:text-base uppercase'>{`${Category}`}</h3>
         </div>
     </NavLink> 
 )}
--- src/Components/Reuse Component/ProductCard.jsx
+++ src/Components/Reuse Component/ProductCard.jsx
@@ -4,22 +4,22 @@ import { NavLink } from 'react-router-dom'
 const Card = (
     {
         id,
-        img,
-        title,
-        price,
-        fit,
+        img="https://www.snitch.co.in/cdn/shop/files/4MST2375-01-M24.jpg?v=1718775027&width=720",
+        colour= "Black",
+        price="1,699",
+        fit="Over size",
     }
 ) => {
   return (
     <NavLink to={`product/${id}`} className='h-72 w-36 '>
         <div className="w-full h-52 overflow-hidden">
-            <img className='w-full h-full object-fill' src="https://www.snitch.co.in/cdn/shop/files/4MST2375-01-M24.jpg?v=1718775027&width=720" alt="" />
+            <img className='w-full h-full object-fill' src={`${img}`} alt="" />
         </div>
         <div className="py-1 px-2">
             <h3 className='text-black font-bol font-1 text-sm'>jet Black Jeans</h3>
-            <h3 className='text-black font-bol font-2 text-sm'> Polo T-Shirt INR 1,699</h3>
-            <h3 className='text-black font-bol font-2 text-sm'>Colour : Black </h3>
-            <h3 className='text-black font-bol font-2 text-sm'>Fit Pant</h3>
+            <h3 className='text-black font-bol font-2 text-sm'> {`INR ${price}`}</h3>
+            <h3 className='text-black font-bol font-2 text-sm'>{`Colour : ${colour}`} </h3>
+            <h3 className='text-black font-bol font-2 text-sm'>{`${fit}`}</h3>
         </div>
 
     </NavLink> 
