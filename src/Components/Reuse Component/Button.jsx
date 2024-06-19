a9068efa41c6be959ede92dd5ad6b08789eab75c
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-19 18:07:51 +0530

    Delete unused font files and update Button component

--- src/Components/Reuse Component/Button.jsx
+++ src/Components/Reuse Component/Button.jsx
@@ -4,14 +4,12 @@ function Button({
     children,
     className= "",
     type = 'button',
-    bgColor = 'bg-blue-600',
-    textColor = 'text-white',
     onClick,
     ...props
     
 }) {
   return (
-    <button className={`px-4 py-2 rounded ${bgColor} ${textColor} ${className}`} {...props}>
+    <button className={`px-4 py-2 rounded ${className}`} {...props}>
         {children}
     </button>
   )
