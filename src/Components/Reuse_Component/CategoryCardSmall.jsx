baccbdf36064c5e81abc406516dc212291ba35a3
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-28 13:46:09 +0530

    Add CategoryCardSmall component and fetch data in Home component

---
+++ src/Components/Reuse_Component/CategoryCardSmall.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CategoryCardSmall = () => {
+  
+  return (
+    <div className="overflow-x-scroll px-5">
+        <div className="h-48 md:h-56 flex gap-3 w-full">
+            <div className="h-full min-w-28 inline-block select-none">
+                <div className="h-[86%] relative">
+                    <div className="h-[40%] w-full rounded "></div>
+                    <div className="h-[60%] w-full rounded bg-slate-300 relative"></div>
+                    <img className='absolute -bottom-3 select-none' src="/img/jj.png" alt="" />
+                    
+                </div>
+                <h5 className='text-sm text-center capitalize font-1'>shirt</h5>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CategoryCardSmall
--- src/Components/WebPages/Home.jsx
+++ src/Components/WebPages/Home.jsx
@@ -1,17 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Reuse_Component/ProductCard';
-import CategoryCard from '../Reuse_Component/CategoryCard';
+import CategoryCardSmall from '../Reuse_Component/CategoryCardSmall';
 import Navbar from '../HeaderFooter/Header';
 import Footer from '../HeaderFooter/Footer';
+import axios from '../../utils/Axios';
+import { useSelector, useDispatch } from'react-redux'
+import {setBestSellerProducts, setSwiperImg, setNewArrivedProducts} from '../../store/homeSlice'
+import Swiper from '../Reuse_Component/Swiper';
+
+
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const [loding, setLoading] = useState(true)
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('/best/');
+        dispatch(setBestSellerProducts(response.data.data))
+        // console.log(response.data.data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('/new/');
+        dispatch(setNewArrivedProducts(response.data.data))
+        // console.log(response.data.data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('/img/');
+        dispatch(setSwiperImg(response.data.data))
+        // console.log(response.data.data)
+        setLoading(false)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  if(loding === true){
+    return <h1>Loading...</h1>
+  }
 
   return (
     <>
     <Navbar/>
-    <div className="flex flex-col pt-20">
-      
+    <div className="flex flex-col pt-16">
+      <Swiper/>
+
+      <CategoryCardSmall/>
+   
 
     </div>
     <Footer/>
